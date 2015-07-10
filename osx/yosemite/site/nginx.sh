39df3c20188151ad5e700fb0ae53fbb11a5f3cf1
Travis Blasingame <travis@Traviss-MacBook-Pro.local>
2015-07-10 12:16:11 -0500

    Fixing nginx conf

--- osx/yosemite/site/nginx.sh
+++ osx/yosemite/site/nginx.sh
@@ -5,7 +5,8 @@
 read -p "What port should this run on? [Hit enter to leave as :80]  " port
 port=${port:-80}
 
-sed -e s/{{PATH}}/"$siteDir"/g -e s/{{PORT}}/"$port"/g ./files/default.template > ./dasher
+PATH=`echo "$siteDir" | sed 's/\//\\\\\//g'`
+sed -e s/{{PATH}}/"$PATH"/g -e s/{{PORT}}/"$port"/g ./files/default.template > ./dasher
 
 # Move it to the proper location
 mv ./dasher /usr/local/etc/nginx/sites-available/dasher
