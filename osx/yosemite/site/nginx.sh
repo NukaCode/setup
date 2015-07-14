05fe30c82d7f5565aebeeb28028f839d42d33e7f
Stygian <stygian.warlock.v2@gmaill.com>
2015-07-14 00:05:03 -0500

    Changing the default port to 1024

--- osx/yosemite/site/nginx.sh
+++ osx/yosemite/site/nginx.sh
@@ -2,8 +2,9 @@
 
 # Create a default virtual host
 # Set up the nginx config
-read -p "What port should this run on? [Hit enter to leave as :80]  " port
-port=${port:-80}
+echo -e "${Yellow}When deciding on a port, remember to stay at or above 1024...$Color_Off"
+read -p "What port should this run on? [Hit enter to leave as :1024]  " port
+port=${port:-1024}
 
 SITE_PATH=`echo "$siteDir" | sed 's/\//\\\\\//g'`
 sed -e s/{{PATH}}/"$SITE_PATH"/g -e s/{{PORT}}/"$port"/g ./files/default.template > ./dasher
