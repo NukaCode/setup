068cb55fb102d3fd27d8ee5cac05872edd6a3b46
Stygian <stygian.warlock.v2@gmaill.com>
2015-07-14 00:06:01 -0500

    Adding an optional step to forward port 80 to dasher's port

--- osx/yosemite/site.sh
+++ osx/yosemite/site.sh
@@ -26,6 +26,15 @@ echo -e "${Green}Cloning the site to $siteDir... $Color_Off"
 echo -e "${Green}Setting up nginx... $Color_Off"
 . site/nginx.sh
 
+read -p "Forward port 80 to $port? [Y/N]?  " portForward
+case $portForward in
+    [yY][eE][sS]|[yY])
+        sudo nginx -s stop
+        echo "rdr pass inet proto tcp from any to any port 80 -> 127.0.0.1 port $port" | sudo pfctl -ef -
+        nginx
+        ;;
+esac
+
 echo -e "${Green}Setting up the database... $Color_Off"
 . site/mysql.sh
 
